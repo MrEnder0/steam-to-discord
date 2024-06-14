d56dba16619b3bf14e0dbdd344ef2c1b7cbb40fe
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2024-06-13 20:31:43 -0500

    FIX

--- main.go
+++ main.go
@@ -9,18 +9,17 @@ func main() {
 	fmt.Println("Starting Steam -> Discord Bridge")
 
 	config := LoadConfig()
-	db := OpenDatabase()
 
-	PopulateDatabase(db)
+	CreateDatabase()
 
 	for {
 		messages := ScrapeSteamGroup(config.GroupName)
 		for _, message := range messages {
-			if FindMessage(db, message.message_id) {
+			if FindMessage(message.message_id) {
 				continue
 			}
 			PostMessage(message, config.WebhookURL, config.ShowSteamPrefix)
-			InsertMessage(db, message)
+			InsertMessage(message.message_id)
 			fmt.Printf("Posted message from: %s\n", message.message_author)
 			time.Sleep(1 * time.Second)
 		}
